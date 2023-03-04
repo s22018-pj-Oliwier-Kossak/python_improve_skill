bf5d1f3dbe249e5748fcae1636571a9541ba9ec9
Oliwier Kossak <oliwierkossak@wp.pl>
2023-03-04 18:02:30 +0100

    list comprehension

---
+++ list_comprehension/list.py
@@ -0,0 +1,10 @@
+
+list1 = [x for x in range(5)]
+list2 = [x**2 for x in range(5)]
+list3 = [x for x in range(10) if x%3 == 0]
+list4 = [x + 1 for x in range(5,11)]
+
+print(list1)
+print(list2)
+print(list3)
+print(list4)
